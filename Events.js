050492ebf9fb85e62402d7735b435c5d3f5d84a4
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-12-08 11:21:50 +0200

    finish the FAQ section Finishing the FAQ section, and add animation to it through observers in js.

--- Events.js
+++ Events.js
@@ -1,3 +1,10 @@
+const faqData = [
+    { question: "What is the BUSD Crops Farmers", answer: "The BUSD Crops Farmer is a decentralized application built on the Binance Smart Chain(BSC). The object of the game is to hire more farmers to help you mine BUSD. These Farmers work for you tirelessly, giving you a daily average of 8% of your farmers' value. The more farmers you hire, the more crops you will get , the more BUSD you will gain" },
+    { question: "how to start mine BUSD and how to withdraw?", answer: `Open busdtminer.info at wallet browser ,choose binance smart chain, Input how much busd you wanna appove, click Appove BUSD, finish the transation (Here gas fee 0.00002BNB about 0.2$), Input how much busd you wanna deposit into contract ,click Hire Farmers.(Here gas fee 0.00002BNB about 0.2$), Wait for farmers working for you ,and you can drink a coffee ,get the Crops after the timer cool down, Click Harverst Crops after 4 hours. BUSD will auto sending to your wallet, you can choose Hire more farmers compound or harvest to withdraw by yourself.` },
+    { question: "what to mention?", answer: "Open the platform at wallet brower (such as trust wallet, metamask), or you can click connect at the corner of website when you connet it at PC browser. Choose binance smart chain(BSC ,BEP20) only . Firstly appove busd then hire farmers." },
+    // Add more FAQ items as needed
+];
+
 document.addEventListener("DOMContentLoaded", function () {
     // Set the initial background position to center for the Introduction section
     document.querySelectorAll(".Introduction").forEach(function (element) {
@@ -16,6 +23,8 @@ document.addEventListener("DOMContentLoaded", function () {
     // Define the elements you want to observe
     const section = document.querySelector('.section');
     const userWallet = document.querySelector('.User-Wallet');
+    const faqSection = document.querySelector(".faq-section");
+    const faqItems = document.querySelectorAll(".faq-item");
 
     // Options for the Intersection Observer
     const options = {
@@ -45,4 +54,78 @@ document.addEventListener("DOMContentLoaded", function () {
     const observerUserWallet = new IntersectionObserver(callback, options);
     // Observe the 'userWallet' element
     observerUserWallet.observe(userWallet);
+
+    // Create an Intersection Observer with the callback and options for the FAQ section
+    const observerFaqSection = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                // Add the class to animate the FAQ section
+                faqSection.classList.add("is-visible");
+                // Unobserve the target to stop observing once it's visible
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    // Observe the FAQ section
+    observerFaqSection.observe(faqSection);
+
+    // Create an Intersection Observer with the callback and options for each FAQ item
+    const observerFaqItems = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                // Add the class to animate the FAQ item
+                entry.target.classList.add("is-visible");
+                // Unobserve the target to stop observing once it's visible
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    // Observe each FAQ item
+    faqItems.forEach((item) => {
+        observerFaqItems.observe(item);
+    });
+
+    const faqCarousel = document.querySelector(".faq-carousel");
+    const faqPrevBtn = document.querySelector(".faq-prev-btn");
+    const faqNextBtn = document.querySelector(".faq-next-btn");
+
+    let currentIndex = 0;
+
+    function showFaqItem(index) {
+        const currentFaq = faqData[index];
+
+        // Create a new FAQ item element
+        const faqItem = document.createElement("div");
+        faqItem.classList.add("faq-item", "is-visible"); // Add is-visible class to the new item
+
+        // Populate the HTML content dynamically
+        faqItem.innerHTML = `
+            <h3>${currentFaq.question}</h3>
+            <p>${currentFaq.answer}</p>
+        `;
+
+        // Insert the new FAQ item into the faq-carousel
+        faqCarousel.innerHTML = '';
+        faqCarousel.appendChild(faqItem);
+    }
+
+    function nextFaqItem() {
+        currentIndex = (currentIndex + 1) % faqData.length;
+        showFaqItem(currentIndex);
+    }
+
+    function prevFaqItem() {
+        currentIndex = (currentIndex - 1 + faqData.length) % faqData.length;
+        showFaqItem(currentIndex);
+    }
+
+    faqPrevBtn.addEventListener("click", prevFaqItem);
+    faqNextBtn.addEventListener("click", nextFaqItem);
+
+    // Initial display
+    showFaqItem(currentIndex);
+
+    setInterval(nextFaqItem, 5000); // Change slide every 5 seconds
 });
